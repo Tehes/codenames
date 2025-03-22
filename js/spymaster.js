8dbb3dbd566a85b57e14f93703d73ba27ffdebaa
Tino Sabetta <tino.sabetta@gmail.com>
2025-03-22 14:00:34 +0100

    Refactor: Simplify card color assignment using forEach and update hash handling for testing

--- js/spymaster.js
+++ js/spymaster.js
@@ -1,28 +1,34 @@
 spymaster = function () {
     function makeBoard() {
-        const hash = location.hash.substring(1);
+        //const hash = location.hash.substring(1);
 
         // for testing purposes
-        // var hash = "0002001101231010201212122";
+        const hash = "2232244342222442434244222";
+        //const hash = "3100122210010112102021020";
 
         const colorIndex = hash.split("");
 
         const colors = ["blue", "red", "neutral", "black", "green"];
         const cards = document.querySelectorAll(".card");
 
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].classList.add(colors[colorIndex[i]]);
-        }
+        cards.forEach((card, i) => {
+            card.classList.add(colors[colorIndex[i]]);
+        });
     }
 
     function nameStarter() {
         const blueCards = document.querySelectorAll(".blue");
         const redCards = document.querySelectorAll(".red");
+        const greenCards = document.querySelectorAll(".green");
 
-        const startingTeam = (blueCards.length === 0 && redCards.length === 0) ? false : ((blueCards.length > redCards.length) ? "Blau" : "Rot");
+        const startingTeam = (blueCards.length > redCards.length) ? "Blau" : "Rot";
 
         const teamVariable = document.querySelector("h1 span");
-        teamVariable.textContent = (startingTeam === false) ? "" : startingTeam;
+        if (greenCards.length > 0) {
+            teamVariable.textContent = "";
+        } else {
+            teamVariable.textContent = startingTeam;
+        }
     }
 
     function init() {
